062a3e037958726498f59397d444280f0961740d
Micha Mazaheri <micha@mazaheri.me>
2013-04-14 18:32:56 +0900

    Added NSObject+TDBindings to add custom bindings.

---
+++ LMTextView/NSObject+TDBindings.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+TDBindings.h
+//  LMTextView
+//
+//  Created by Micha Mazaheri on 4/14/13.
+//  Copyright (c) 2013 Lucky Marmot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (TDBindings)
+
+-(void) propagateValue:(id)value forBinding:(NSString*)binding;
+
+@end
